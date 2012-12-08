30280774933977a818e78465cb4ccdee49dfcbcb
Alex Zaporozhets <alexzaporozhets@gmail.com>
2012-12-08 17:36:40 +0200

    added /sitemap.xml URL trigger

--- core/urls.py
+++ core/urls.py
@@ -2,10 +2,12 @@ from django.conf.urls import patterns, include, url
 from django.conf import settings
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from cms.sitemaps import CMSSitemap
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': {'cmspages': CMSSitemap}}),
     url(r'^', include('cms.urls')),
 )
 
