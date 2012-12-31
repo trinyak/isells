03b7a4f6f03dc9ccbb31b13b0c7d8b917358da33
Alex Zaporozhets <alexzaporozhets@gmail.com>
2012-12-31 15:58:28 +0200

    fix path for site-creation script

--- shop/tasks.py
+++ shop/tasks.py
@@ -5,7 +5,7 @@ import os;
 
 @task()
 def addWebsite(site):
-    os.system(settings.PROJECT_DIR + '../scripts/create-site.sh ' + site.slug)
+    os.system(settings.PROJECT_DIR + '/../scripts/create-site.sh ' + site.slug)
     site.created = True;
     site.save()
     return site
